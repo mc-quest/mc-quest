817fa3821b2077cfbe3f655d6634ea60bab2a49b
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2023-02-18 17:55:02 -0800

    Skills

--- server/src/main/java/com/mcquest/server/features/FighterPlayerClass.java
+++ server/src/main/java/com/mcquest/server/features/FighterPlayerClass.java
@@ -1,10 +1,23 @@
 package com.mcquest.server.features;
 
 import com.mcquest.server.Mmorpg;
+import com.mcquest.server.character.Character;
+import com.mcquest.server.character.CharacterHitbox;
 import com.mcquest.server.character.PlayerCharacter;
 import com.mcquest.server.constants.FighterSkills;
 import com.mcquest.server.event.ActiveSkillUseEvent;
+import com.mcquest.server.event.PlayerCharacterLoginEvent;
 import com.mcquest.server.feature.Feature;
+import com.mcquest.server.instance.Instance;
+import com.mcquest.server.physics.Collider;
+import com.mcquest.server.physics.PhysicsManager;
+import com.mcquest.server.util.ParticleEffects;
+import net.kyori.adventure.sound.Sound;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.coordinate.Vec;
+import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.particle.Particle;
+import net.minestom.server.sound.SoundEvent;
 
 public class FighterPlayerClass implements Feature {
     private Mmorpg mmorpg;
@@ -12,11 +25,47 @@ public class FighterPlayerClass implements Feature {
     @Override
     public void hook(Mmorpg mmorpg) {
         this.mmorpg = mmorpg;
+        GlobalEventHandler eventHandler = mmorpg.getGlobalEventHandler();
+        eventHandler.addListener(PlayerCharacterLoginEvent.class, this::handleLevelUp);
         FighterSkills.BASH.onUse().subscribe(this::useBash);
         FighterSkills.SELF_HEAL.onUse().subscribe(this::useSelfHeal);
     }
 
+    private void handleLevelUp(PlayerCharacterLoginEvent event) {
+        PlayerCharacter pc = event.getPlayerCharacter();
+        pc.setMaxMana(100);
+        pc.setMana(100);
+    }
+
     private void useBash(ActiveSkillUseEvent event) {
+        PlayerCharacter pc = event.getPlayerCharacter();
+        Instance instance = pc.getInstance();
+        Pos hitboxCenter = pc.getEyePosition().add(pc.getLookDirection().mul(1.5));
+        Vec hitboxSize = new Vec(1.5, 1.5, 1.5);
+        Collider hitbox = new Collider(instance, hitboxCenter, hitboxSize);
+        boolean[] hitOccurred = {false};
+        hitbox.onCollisionEnter(other -> {
+            if (!(other instanceof CharacterHitbox characterHitbox)) {
+                return;
+            }
+            Character character = characterHitbox.getCharacter();
+            if (character.isFriendly(pc)) {
+                return;
+            }
+            hitOccurred[0] = true;
+            double damageAmount = 5.0;
+            character.damage(pc, damageAmount);
+            Sound hitSound = Sound.sound(SoundEvent.ENTITY_ZOMBIE_ATTACK_IRON_DOOR, Sound.Source.PLAYER, 1f, 1f);
+            instance.playSound(hitSound, other.getCenter());
+            ParticleEffects.particle(instance, hitboxCenter, Particle.CLOUD);
+        });
+        PhysicsManager physicsManager = mmorpg.getPhysicsManager();
+        physicsManager.addCollider(hitbox);
+        physicsManager.removeCollider(hitbox);
+        if (!hitOccurred[0]) {
+            Sound missSound = Sound.sound(SoundEvent.ENTITY_WITHER_SHOOT, Sound.Source.PLAYER, 1f, 1.5f);
+            instance.playSound(missSound);
+        }
     }
 
     private void useSelfHeal(ActiveSkillUseEvent event) {
--- server/src/main/java/com/mcquest/server/features/MagePlayerClass.java
+++ server/src/main/java/com/mcquest/server/features/MagePlayerClass.java
@@ -1,9 +1,22 @@
 package com.mcquest.server.features;
 
 import com.mcquest.server.Mmorpg;
+import com.mcquest.server.character.Character;
+import com.mcquest.server.character.CharacterHitbox;
+import com.mcquest.server.character.PlayerCharacter;
 import com.mcquest.server.constants.MageSkills;
 import com.mcquest.server.event.ActiveSkillUseEvent;
 import com.mcquest.server.feature.Feature;
+import com.mcquest.server.instance.Instance;
+import com.mcquest.server.physics.Collider;
+import com.mcquest.server.physics.PhysicsManager;
+import net.kyori.adventure.sound.Sound;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.coordinate.Vec;
+import net.minestom.server.entity.Entity;
+import net.minestom.server.entity.EntityType;
+import net.minestom.server.event.player.PlayerChatEvent;
+import net.minestom.server.sound.SoundEvent;
 
 public class MagePlayerClass implements Feature {
     private Mmorpg mmorpg;
@@ -12,8 +25,47 @@ public class MagePlayerClass implements Feature {
     public void hook(Mmorpg mmorpg) {
         this.mmorpg = mmorpg;
         MageSkills.FIREBALL.onUse().subscribe(this::useFireball);
+        mmorpg.getGlobalEventHandler().addListener(PlayerChatEvent.class, evt -> {
+            PlayerCharacter pc = mmorpg.getPlayerCharacterManager().getPlayerCharacter(evt.getPlayer());
+            useFireball(new ActiveSkillUseEvent(pc, null));
+        });
     }
 
-    private void useFireball(ActiveSkillUseEvent event) {
+    public void useFireball(ActiveSkillUseEvent event) {
+        PlayerCharacter pc = event.getPlayerCharacter();
+        Instance instance = pc.getInstance();
+        Pos startPosition = pc.getEyePosition().add(pc.getLookDirection().mul(1f));
+        Vec hitboxSize = new Vec(1f, 1f, 1f);
+        Collider hitbox = new Collider(instance, startPosition, hitboxSize);
+        Entity fireballEntity = new Entity(EntityType.FIREBALL) {
+            @Override
+            public void tick(long time) {
+                super.tick(time);
+                hitbox.setCenter(this.getPosition());
+            }
+        };
+        PhysicsManager physicsManager = mmorpg.getPhysicsManager();
+        hitbox.onCollisionEnter(other -> {
+            if (!(other instanceof CharacterHitbox characterHitbox)) {
+                return;
+            }
+            Character character = characterHitbox.getCharacter();
+            if (character.isFriendly(pc)) {
+                return;
+            }
+            double damageAmount = 6;
+            character.damage(pc, damageAmount);
+            fireballEntity.remove();
+            physicsManager.removeCollider(hitbox);
+            Sound hitSound = Sound.sound(SoundEvent.ENTITY_DRAGON_FIREBALL_EXPLODE, Sound.Source.PLAYER, 1f, 1f);
+            instance.playSound(hitSound);
+        });
+        physicsManager.addCollider(hitbox);
+        Vec fireballVelocity = pc.getLookDirection().mul(20.0);
+        fireballEntity.setInstance(instance, startPosition).join();
+        fireballEntity.setNoGravity(true);
+        fireballEntity.setVelocity(fireballVelocity);
+        Sound summonFireballSound = Sound.sound(SoundEvent.BLOCK_FIRE_EXTINGUISH, Sound.Source.PLAYER, 1f, 1f);
+        instance.playSound(summonFireballSound);
     }
 }

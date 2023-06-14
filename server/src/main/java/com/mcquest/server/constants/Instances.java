ca62aea0c0ed8785230eefbcb37fae3467147a16
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2023-06-14 00:15:37 -0700

    Bulskan Ruins test instance

--- server/src/main/java/com/mcquest/server/constants/Instances.java
+++ server/src/main/java/com/mcquest/server/constants/Instances.java
@@ -7,10 +7,14 @@ public class Instances {
     public static final Instance ELADRADOR = Instance.builder(1)
             .chunkLoader(new AnvilLoader("world/eladrador"))
             .build();
+    public static final Instance BULSKAN_RUINS = Instance.builder(2)
+            .chunkLoader(new AnvilLoader("world/bulskan_ruins"))
+            .build();
 
     public static Instance[] all() {
         return new Instance[]{
-                ELADRADOR
+                ELADRADOR,
+                BULSKAN_RUINS
         };
     }
 }
--- server/src/main/java/com/mcquest/server/features/TestFeature.java
+++ server/src/main/java/com/mcquest/server/features/TestFeature.java
@@ -2,29 +2,87 @@ package com.mcquest.server.features;
 
 import com.mcquest.server.Mmorpg;
 import com.mcquest.server.character.PlayerCharacter;
-import com.mcquest.server.constants.Instances;
-import com.mcquest.server.constants.Items;
-import com.mcquest.server.constants.Maps;
-import com.mcquest.server.constants.Positions;
+import com.mcquest.server.commerce.Money;
+import com.mcquest.server.constants.*;
 import com.mcquest.server.event.PlayerCharacterLoginEvent;
+import com.mcquest.server.event.PlayerCharacterMoveEvent;
 import com.mcquest.server.feature.Feature;
+import com.mcquest.server.item.Item;
+import com.mcquest.server.item.PlayerCharacterInventory;
 import com.mcquest.server.loot.*;
+import com.mcquest.server.physics.Collider;
+import com.mcquest.server.util.Debug;
 import net.minestom.server.attribute.Attribute;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.coordinate.Vec;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.player.PlayerChatEvent;
 import net.minestom.server.timer.SchedulerManager;
 
+import java.io.File;
 import java.time.Duration;
+import java.util.Map;
 
 public class TestFeature implements Feature {
     @Override
     public void hook(Mmorpg mmorpg) {
+        mmorpg.getGlobalEventHandler().addListener(PlayerCharacterMoveEvent.class, event -> {
+            PlayerCharacter pc = event.getPlayerCharacter();
+            PlayerCharacterInventory inventory = pc.getInventory();
+            Map<Item, Integer> items = Map.of(Items.TEST_ITEM, 2239);
+            Map<Item, Integer> items2 = Map.of(Items.TEST_ITEM, 2240);
+        });
+        Collider eladradorTrigger = new Collider(Instances.ELADRADOR, new Pos(5, 72, 5), new Vec(5));
+        Collider bulskanTrigger = new Collider(Instances.BULSKAN_RUINS, new Pos(0, -56, 0), new Vec(5));
+        eladradorTrigger.onCollisionEnter(other -> {
+            if (other instanceof PlayerCharacter.Hitbox hitbox) {
+                PlayerCharacter pc = hitbox.getCharacter();
+                pc.setPosition(new Pos(5, -58, 5));
+                pc.setInstance(Instances.BULSKAN_RUINS);
+                pc.setZone(Zones.BULSKAN_RUINS);
+                pc.getMusicPlayer().setSong(Music.DUNGEON);
+            }
+        });
+        bulskanTrigger.onCollisionEnter(other -> {
+            if (other instanceof PlayerCharacter.Hitbox hitbox) {
+                PlayerCharacter pc = hitbox.getCharacter();
+                pc.setPosition(new Pos(0, 69, 0));
+                pc.setInstance(Instances.ELADRADOR);
+                pc.setZone(Zones.OAKSHIRE);
+                pc.getMusicPlayer().setSong(Music.WILDERNESS);
+            }
+        });
+        mmorpg.getPhysicsManager().addCollider(eladradorTrigger);
+        mmorpg.getPhysicsManager().addCollider(bulskanTrigger);
+        Debug.showCollider(eladradorTrigger);
+        Debug.showCollider(bulskanTrigger);
+        mmorpg.getGlobalEventHandler().addListener(PlayerChatEvent.class, event -> {
+            Player player = event.getPlayer();
+            PlayerCharacter pc = mmorpg.getPlayerCharacterManager().getPlayerCharacter(player);
+            pc.getInventory().add(Items.ADVENTURERS_SWORD);
+            //            Instance newInstance;
+//            Pos newPosition;
+//            if (pc.getInstance() == Instances.ELADRADOR) {
+//                newInstance = Instances.BULSKAN_RUINS;
+//                newPosition = new Pos(0, -59, 0);
+//            } else {
+//                newInstance = Instances.ELADRADOR;
+//                newPosition = new Pos(0, 69, 0);
+//            }
+//            pc.setInstance(newInstance);
+//            pc.setPosition(newPosition);
+        });
+
+
+        mmorpg.getResourcePackManager().writeResourcePack(new File("resourcepack.zip"));
         LootChestManager lootChestManager = mmorpg.getLootChestManager();
         SchedulerManager scheduler = mmorpg.getSchedulerManager();
         LootTable lootTable = LootTable.builder()
                 .pool(Pool.builder()
-                        .entry(ItemPoolEntry.builder(Items.TEST_ITEM).build())
-                        .entry(MoneyPoolEntry.builder().build())
+                        .rolls(100)
+                        .entry(ItemPoolEntry.builder(Items.TEST_ITEM).amount(4).build())
+                        .entry(MoneyPoolEntry.builder().value(Money.copper(3), Money.copper(20)).weight(2).build())
                         .build())
                 .build();
         LootChest lootChest = new LootChest(Instances.ELADRADOR, Positions.SPAWN_POSITION, lootTable);
@@ -37,13 +95,19 @@ public class TestFeature implements Feature {
         eventHandler.addListener(PlayerCharacterLoginEvent.class, this::handleLevelUp);
         mmorpg.getGlobalEventHandler().addListener(PlayerCharacterLoginEvent.class, event -> {
             PlayerCharacter pc = event.getPlayerCharacter();
-            pc.giveItem(Items.TEST_ITEM);
+            pc.getInventory().add(Items.TEST_ITEM);
             pc.getMapManager().setMap(Maps.MELCHER);
             Player player = pc.getPlayer();
-            player.getAttribute(Attribute.MOVEMENT_SPEED).setBaseValue(1);
+            // player.getAttribute(Attribute.MOVEMENT_SPEED).setBaseValue(1);
             player.getAttribute(Attribute.FLYING_SPEED).setBaseValue(1);
             player.setAllowFlying(true);
         });
+//        BufferedImage map = MapMaker.createMap(Instances.ELADRADOR, -1000, -500, 1000, 1000, 80);
+//        try {
+//            ImageIO.write(map, "png", new File("map.png"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 
     private void handleLevelUp(PlayerCharacterLoginEvent event) {
@@ -51,5 +115,4 @@ public class TestFeature implements Feature {
         pc.setMaxMana(100);
         pc.setMana(100);
     }
-
 }

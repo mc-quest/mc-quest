fe2183a956f3b24bf28c45720493ec8189f8aaed
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2023-07-09 12:17:17 -0700

    Delete achievements for now

---
+++ core/src/main/java/com/mcquest/core/resourcepack/GuiCustomModelData.java
@@ -0,0 +1,11 @@
+package com.mcquest.core.resourcepack;
+
+import org.jetbrains.annotations.ApiStatus;
+
+@ApiStatus.Internal
+public class GuiCustomModelData {
+    public static final int HOTBAR_SKILL_PLACEHOLDER = 1;
+    public static final int HOTBAR_SKILL_PLACEHOLDER_FLASHING = 2;
+    public static final int HOTBAR_CONSUMABLE_PLACEHOLDER = 3;
+    public static final int HOTBAR_CONSUMABLE_FLASHING = 4;
+}

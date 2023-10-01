91ed0296cff3b001b29dd64e699169e68b9da3da
Connor Reinholdtsen <connorreinholdtsen@outlook.com>
2023-10-01 14:21:17 -0700

    Music

--- server/src/main/java/com/mcquest/server/constants/Music.java
+++ server/src/main/java/com/mcquest/server/constants/Music.java
@@ -8,12 +8,14 @@ public class Music {
     public static final Song WILDERNESS = new Song(1, audio("wilderness"));
     public static final Song DUNGEON = new Song(2, audio("dungeon"));
     public static final Song BROODMOTHER_LAIR = new Song(3, audio("broodmother_lair"));
+    public static final Song BROODMOTHER_BATTLE = new Song(4, audio("broodmother_battle"));
 
     public static Song[] all() {
         return new Song[]{
                 WILDERNESS,
                 DUNGEON,
-                BROODMOTHER_LAIR
+                BROODMOTHER_LAIR,
+                BROODMOTHER_BATTLE
         };
     }
 

156f54cabc57eae3015324767605886bff1c5913
Kyle Reinholdtsen <kyler56@uw.edu>
2023-11-07 15:48:58 -0800

    Create unimplmented skill

--- server/src/main/java/net/mcquest/server/constants/MageSkills.java
+++ server/src/main/java/net/mcquest/server/constants/MageSkills.java
@@ -5,4 +5,5 @@ import net.mcquest.core.playerclass.ActiveSkill;
 public class MageSkills {
     public static final ActiveSkill FIREBALL = (ActiveSkill) PlayerClasses.MAGE.getSkill("fireball");
     public static final ActiveSkill ICE_BEAM = (ActiveSkill) PlayerClasses.MAGE.getSkill("ice_beam");
+    public static final ActiveSkill CHAIN_LIGHTNING = (ActiveSkill) PlayerClasses.MAGE.getSkill("chain_lightning");
 }
